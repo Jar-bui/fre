342d1b532c0e4c2b157222e2caaa2bc841f671c9
勇士 <7962573+warriorTrue@user.noreply.gitee.com>
2020-09-30 11:45:57 +0800

    dishan

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import Flo from './Flo'
+import axios from 'axios'
 
 class App extends React.Component{
+  componentDidMount(){
+    axios.get('/api/db/in_theaters').then(res=>{
+      console.log(res)
+    })
+  }
   render(){
     return(
       <div className="App">
--- src/Flo.js
+++ src/Flo.js
@@ -46,8 +46,34 @@ export default class Flo extends Component {
             })
         }
         dara = (id)=>{
-            console.log(id)
+            // console.log(id)
+            axios.delete(`http://localhost:4000/aoihoih/${id}`).then(res=>{
+                console.log("成功")
+                this.sett()
+            })
 
+        }
+        up = (item)=>{
+            let app = prompt("输入",item.title+","+item.author).split(",")
+            axios.patch(`http://localhost:4000/aoihoih/${item.id}`,{
+                title:app[0],
+                author:app[1]
+            }).then(res=>{
+                console.log('成功')
+                this.sett()
+            })
+            console.log(app)
+        }
+        sercH = ()=>{
+            
+            axios.get(`http://localhost:4000/aoihoih?title_like=`+this.state.user).then(res=>{
+                // console.log(res)
+                this.setState({
+                    list:res.data
+                })
+                // this.sett()
+                console.log(this.state.user)
+            })
         }
         render() {
             let {list,user,ax} = this.state
@@ -58,11 +84,12 @@ export default class Flo extends Component {
                 <input id="user" value={user} onChange={this.hand}/>
                 <input id="ax" value={ax} onChange={this.hand}/>
                 <button type="" onClick={this.handJ}>添加</button>
+                <button type="" onClick={this.sercH}>查询</button>
                 {
                     list.map((item,index)=>{
                     return <li key={index}>{item.id} {item.author}{item.title}
                         <button type="" onClick={this.dara.bind(this,item.id)}>删除</button>
-                        <button type="" >修改</button>
+                        <button type="" onClick={this.up.bind(this,item)}>修改</button>
                     </li>
                     })
                 }
